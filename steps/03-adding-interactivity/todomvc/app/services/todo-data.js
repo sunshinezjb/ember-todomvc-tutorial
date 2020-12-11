826afe7a5bc4b6298688fc574836804bd12ab263
sunshinezjb <zhengjunbo68@126.com>
2020-12-11 22:47:37 +0800

    init origin project

---
+++ steps/03-adding-interactivity/todomvc/app/services/todo-data.js
@@ -0,0 +1,47 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+class Todo {
+  @tracked text = '';
+  @tracked isCompleted = false;
+
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+export default class TodoDataService extends Service {
+  @tracked todos = [];
+
+  get all() {
+    return this.todos;
+  }
+
+  get incomplete() {
+    return this.todos.filter(todo => {
+      return todo.isCompleted === false;
+    });
+  }
+
+  get completed() {
+    return this.todos.filter(todo => todo.isCompleted);
+  }
+
+  @action
+  add(text) {
+    let newTodo = new Todo(text);
+
+    this.todos = [...this.todos, newTodo];
+  }
+
+  @action
+  clearCompleted() {
+    this.todos = this.incomplete;
+  }
+
+  @action
+  toggleCompletion(todo) {
+    todo.isCompleted = !todo.isCompleted;
+  }
+}
